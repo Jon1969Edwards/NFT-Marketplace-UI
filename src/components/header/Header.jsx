82a1142ef1711a129bb1fcdd2b1fa61671ff23c9
Jonathan C Edwards <Jon1969edwards@gmail.com>
2022-11-22 10:59:59 +0100

    Changes bg img and header title card

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,17 +2,26 @@ import React from 'react'
 import './header.css'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import coin from '../../assets/coin.png'
+
 const Header = () => {
   
   return (
     <div className='header section__padding'>
-      <div className="header-content">
+      <div className='header-container'>
+        <section className='neon-block'>
+          <div className='block'>
+            <span className='rainbow'></span>
+            Win! Win! Win!
+          </div>
+        </section>
+      </div>
+      {/* <div className="header-content">
         <div>
           <h1>Win, Win, Win!!!</h1>
           <img className='shake-vertical' src={coin} alt="" />
         </div>
-      </div>
+        // import coin from '../../assets/coin.png'
+      </div> */}
       
     </div>
   )
