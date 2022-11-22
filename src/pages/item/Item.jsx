8c646d786f9c88a24e47cdf9fcb00bdd61cf4d0a
Jonathan C Edwards <Jon1969edwards@gmail.com>
2022-11-22 11:22:57 +0100

    Adjusts logo size and fixes link to 'slots' page

--- src/pages/item/Item.jsx
+++ src/pages/item/Item.jsx
@@ -29,7 +29,7 @@ const Item = () => {
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
             </div>
             <div className="item-content-buy">
-              <Link to={`/slots/Slots.jsx`}>
+              <Link to={`/pages/slots/Slots.jsx`}>
                 <button className="primary-btn">Play For 4.5 ETH</button>
               </Link>
               <Link>
